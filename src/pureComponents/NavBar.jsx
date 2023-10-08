171e767c4ff28129ab1851c48aad042a4044e8f3
Daniel Aurelio Muñoz <ryuhirakiyuki@outlook.com>
2023-10-07 20:23:44 -0600

    DAMG: little changes here, idk

---
+++ src/pureComponents/NavBar.jsx
@@ -0,0 +1,14 @@
+import CVpdf from "../assets/CV - Muñoz González Daniel Aurelio.pdf"
+
+export default function NavBar(){
+    return(
+        <div className="flex flex-row justify-between p-5">
+            <div>Daniel Aurelio Muñoz González</div>
+            <a className="bg-secAmar w-[10%]" href={CVpdf} target="_blank" download={CVpdf} rel="noreferrer">
+                <div >
+                    Descargar CV
+                </div>
+            </a>
+        </div>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        bgGeneral:"#4B644A",
+        secAmar:"#FBB13C",
+        secRoj:"#98473E",
+        secRos:"#FFA3AF"
+      },
+    },
+  },
+  plugins: [],
+}
+
